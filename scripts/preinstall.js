716146248fc43fbe2c0be358f26c708c16b358e7
DKartsev <50330328+DKartsev@users.noreply.github.com>
2025-08-10 15:28:26 +0400

    fix: make preinstall cross-platform

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,1 +1,6 @@
-console.log("[preinstall] no-op script for production build");
+if (process.env.NODE_ENV === 'production') {
+  console.log('[preinstall] skipping script in production environment');
+  process.exit(0);
+}
+
+console.log('[preinstall] no-op script for non-production environments');
